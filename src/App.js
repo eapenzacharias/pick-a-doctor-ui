effe28a3cc1f001157823ec421b18ad7259e05f6
eapenzacharias <eapenzac@gmail.com>
2022-05-14 01:35:32 +0200

    Added router

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { Routes, Route } from 'react-router-dom';
+import SignInForm from './components/authPages/SignInForm';
+import SignUpForm from './components/authPages/SignUpForm';
 import LandingView from './components/LandingView/LandingView';
 import SideNav from './components/SideNav/SideNav';
 
@@ -5,7 +8,11 @@ function App() {
   return (
     <>
       <SideNav />
-      <LandingView />
+      <Routes>
+        <Route path="/" element={<LandingView />} />
+        <Route path="signin" element={<SignInForm />} />
+        <Route path="signup" element={<SignUpForm />} />
+      </Routes>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/stylesheets/index.css';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import { store } from './redux/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 );
