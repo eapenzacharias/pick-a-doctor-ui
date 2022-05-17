a3a5852c500167c00415401d3bc42b79b2bc37be
Maha Magdy <mahamagdy3@gmail.com>
2022-05-17 13:37:33 +0200

    Add the specializations to the side nav as a option

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+// import { Link } from 'react-router-dom';
 import { MenuIcon, XIcon } from '@heroicons/react/outline';
 import styles from './SideNav.module.css';
 
@@ -24,12 +25,22 @@ const SideNav = () => {
         }`}
       >
         <button
-          className={`right-9 top-9 ${styles.closeBtn}`}
+          className={`right-3 top-3 ${styles.closeBtn}`}
           type="button"
           onClick={() => closeNav()}
         >
-          <XIcon className={`h-9 w-9 text-black ${styles.basicColor}`} />
+          <XIcon className={`h-8 w-8 text-black ${styles.basicColor}`} />
         </button>
+        <div className={sideNav === 'close' ? 'hidden' : 'block'}>
+          <h4 className="mt-14 mb-36 ml-4 font-sans text-xl font-bold text-dark_main_color whitespace-nowrap">
+            PICK A DOCTOR
+          </h4>
+          <ul>
+            <li className="p-4 font-sans text-xl font-bold text-dark_main_color cursor-pointer hover:bg-light_main_color hover:text-white">
+              SPECIALIZATIONS
+            </li>
+          </ul>
+        </div>
       </div>
       <button
         type="button"
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark_main_color: '#304656',
+        light_main_color: '#b2dfff'
+      },
+    },
   },
   plugins: [],
 };
